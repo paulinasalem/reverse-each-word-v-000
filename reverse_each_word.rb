7b60af351cab41f3d5053021e0cc2416d78e67e3
Paulina Salem <paulinasalem@yahoo.com>
2018-04-20 20:25:40 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,16 @@
+def reverse_each_word(string)
+  my_reverse = []
+  new_string=string.split
+  new_string.each do |reverse_it|
+    my_reverse<< reverse_it.reverse
+  end
+my_reverse.join(' ')
+end
+
+def reverse_each_word(string)
+  new_string=string.split
+  new_variable=new_string.collect do |reverse_it|
+   reverse_it.reverse
+  end
+  new_variable.join(' ')
+end
