89c68d6f939a8975b80df7ddd08282a1507b3976
Paulina Salem <paulinasalem@yahoo.com>
2018-04-20 20:03:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,6 @@ def reverse_each_word(string)
   new_string=string.split
   new_string.each do |reverse_it|
     my_reverse<< reverse_it.reverse
-    my_reverse.join
   end
-my_reverse
+my_reverse.join
 end
