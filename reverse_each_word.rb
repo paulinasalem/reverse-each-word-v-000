114bd959049410f58e9583af183f3790a46f7ded
Paulina Salem <paulinasalem@yahoo.com>
2018-04-20 19:59:09 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
   my_reverse = []
   new_string=string.split
   new_string.each do |reverse_it|
-    my_reverse<< reverse_it.reverse
-    my_reverse.join
-  end
 
+  end
+  my_reverse<< reverse_it.reverse
+  my_reverse.join
 end
